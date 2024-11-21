e2649d6274ef466ad74b0897a92c73513dce5333
José Augusto <133225201+joseaugustogoncalvesdeoliveira@users.noreply.github.com>
2024-11-21 13:42:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,64 @@
+
+
+criaCartao(
+    '1:Programação',
+    '. O que é Python?',
+    'R: O Python é uma linguagem de programação.'
+)
+criaCartao(
+    '2:Geografia',
+    '. Quantos continentes existem no mundo e quais são eles?',
+    'R: Existem 7 continentes: África, América, Antártica, Ásia, Europa, Oceania e América do Sul.'
+)
+
+criaCartao(
+    '3:Lingua inglesa',
+    '. Como se diz casa em Inglês?',
+   'R: Casa em ingles é house.'
+)
+
+criaCartao(
+    '4:Lingua portuguesa',
+    '. O que é uma metáfora?',
+    'R: Metáfora é uma figura de linguagem que estabelece uma comparação implícita entre dois elementos, sem usar palavras como "como" ou "tal qual".'
+)
+
+criaCartao(
+    '5:Química ',
+    '. Quais são os três estados físicos mais comuns da matéria?',
+    'R: Sólido, líquido e gasoso.'
+)
+
+criaCartao(
+    '6:Educação Financeira',
+    '. O que é educação financeira?',
+    'R: Educação financeira é o conjunto de conhecimentos e habilidades que ajudam a gerir recursos financeiros de forma eficiente, permitindo planejamento, controle de gastos e investimentos para alcançar objetivos pessoais.'
+)
+
+criaCartao(
+    '7:Emprendedorismo',
+    '. O que é um plano de negócios e sua importância?',
+    'R: Um plano de negócios descreve objetivos, estratégias e finanças da empresa, sendo fundamental para guiar o negócio e atrair investidores.'
+
+)
+
+criaCartao(
+    '8:Sociologia',
+    '. O que é desigualdade social?',
+    'R: Desigualdade social é a distribuição desigual de recursos e oportunidades entre diferentes grupos na sociedade.'
+
+)
+
+criaCartao(
+    '9:Matematica',
+    '. Qual é a soma de 125 e 378?',
+    'R: A soma e 503.'
+
+)
+
+criaCartao(
+    '10:Projeto de Vida',
+    '. Como estabelecer metas no meu projeto de vida?',
+    'R: As metas devem ser específicas, mensuráveis, alcançáveis, relevantes e com prazos definidos (critérios SMART). Exemplo: "Quero fazer um curso de inglês em 6 meses."'
+
+)
